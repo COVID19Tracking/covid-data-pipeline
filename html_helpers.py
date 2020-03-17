35a63ca9c26829a487fcb8fe74ef381df5b242e3
Joshua Ellinger <josh@exemplartech.com>
2020-03-17 08:10:17 -0500

    fixed source link

--- html_helpers.py
+++ html_helpers.py
@@ -2,24 +2,27 @@
 from lxml import html
 
 def make_source_link(kind: str, stage: str, name: str) -> html.Element:
-    d = html.Element("div")
-    if kind == stage:
+    d = html.Element("span")
+    if kind != stage and kind != "source":
         a = html.Element("a")
         # "http://covid19-api.exemplartech.com/github-data/raw/AZ.html
-        a.href = f"../{stage}/{name}"
+        a.attrib["href"] = f"../{stage}/{name}"
         a.text = stage
         d.append(a)
     else:
         d.text = stage
-    d.tail = " > "        
+    d.tail = " < "        
     return d
 
 def make_source_links(kind: str, name: str, source: str):
 
     div = html.Element("div")
     div.attrib["class"] = "source"
-            
+
     kind = kind.lower()
+    if not kind in ["extract", "clean", "raw", "source"]: 
+        raise Exception("Invalid kind: " + kind)
+
     d = make_source_link(kind, "extract", name)
     div.append(d)
     d = make_source_link(kind, "clean", name)
@@ -27,6 +30,7 @@ def make_source_links(kind: str, name: str, source: str):
     d = make_source_link(kind, "raw", name)
     div.append(d)
     d = make_source_link(kind, source, name)
+    d.tail = ""
     div.append(d)
 
     return div        
